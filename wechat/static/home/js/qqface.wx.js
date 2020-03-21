18589ba736f293c4ba224a2ecd7538c7f588a410
fen.wen <fen.wen@56linked.com>
2020-03-21 19:45:27 +0800

    添加

--- wechat/static/home/js/qqface.wx.js
+++ wechat/static/home/js/qqface.wx.js
@@ -1,116 +1,117 @@
-// QQ琛ㄦ儏鎻掍欢
+// QQ表情插件
 (function ($) {
-  $.fn.qqFace = function (option) {
-      var settings = $.extend({
-              id: "face-" + new Date().valueOf(),
-              assign: 'textbox',
-              appendbox: '.bottom-bar',
-              path: '/images/newface'
-          },
-          option);
+    $.fn.qqFace = function (option) {
+        var settings = $.extend({
+                id: "face-" + new Date().valueOf(),
+                assign: 'textbox',
+                appendbox: '.bottom-bar',
+                path: '/images/newface'
+            },
+            option);
 
-      return this.each(function () {
-          var tab_id_1 = "tab-" + new Date().valueOf(),
-              tab_id_2 = "tab-" + new Date().valueOf() + 1;
-          var assign = $('#' + settings.assign);
-          var id = settings.id;
+        return this.each(function () {
+            var tab_id_1 = "tab-" + new Date().valueOf(),
+                tab_id_2 = "tab-" + new Date().valueOf() + 1;
+            var assign = $('#' + settings.assign);
+            var id = settings.id;
 
-          function insertHtmlAtCaret(html) {
-              var sel, range;
-              if (window.getSelection) {
-                  // IE9 and non-IE
-                  sel = window.getSelection();
-                  if (sel.getRangeAt && sel.rangeCount) {
-                      range = sel.getRangeAt(0);
-                      range.deleteContents();
+            function insertHtmlAtCaret(html) {
+                var sel, range;
+                if (window.getSelection) {
+                    // IE9 and non-IE
+                    sel = window.getSelection();
+                    if (sel.getRangeAt && sel.rangeCount) {
+                        range = sel.getRangeAt(0);
+                        range.deleteContents();
 
-                      var el = document.createElement("div");
-                      el.innerHTML = html;
-                      var frag = document.createDocumentFragment(), node, lastNode;
-                      while ((node = el.firstChild)) {
-                          lastNode = frag.appendChild(node);
-                      }
-                      range.insertNode(frag);
+                        var el = document.createElement("div");
+                        el.innerHTML = html;
+                        var frag = document.createDocumentFragment(), node, lastNode;
+                        while ((node = el.firstChild)) {
+                            lastNode = frag.appendChild(node);
+                        }
+                        range.insertNode(frag);
 
-                      // Preserve the selection
-                      if (lastNode) {
-                          range = range.cloneRange();
-                          range.setStartAfter(lastNode);
-                          range.collapse(true);
-                          sel.removeAllRanges();
-                          sel.addRange(range);
-                      }
-                  }
-              } else if (document.selection && document.selection.type != "Control") {
-                  // IE < 9
-                  document.selection.createRange().pasteHTML(html);
-              }
-          }
+                        // Preserve the selection
+                        if (lastNode) {
+                            range = range.cloneRange();
+                            range.setStartAfter(lastNode);
+                            range.collapse(true);
+                            sel.removeAllRanges();
+                            sel.addRange(range);
+                        }
+                    }
+                } else if (document.selection && document.selection.type != "Control") {
+                    // IE < 9
+                    document.selection.createRange().pasteHTML(html);
+                }
+            }
 
-          var str = '',
-              QQFaceList = ["寰瑧", "鎾囧槾", "鑹�", "鍙戝憜", "寰楁剰", "娴佹唱", "瀹崇緸", "闂槾", "鐫�", "澶у摥", "灏村艾", "鍙戞€�", "璋冪毊", "鍛茬墮", "鎯婅", "闅捐繃", "閰�", "鍐锋睏", "鎶撶媯", "鍚�", "鍋风瑧", "鎰夊揩", "鐧界溂", "鍌叉參", "楗ラタ", "鍥�", "鎯婃亹", "娴佹睏", "鎲ㄧ瑧", "鎮犻棽", "濂嬫枟", "鍜掗獋", "鐤戦棶", "鍢�", "鏅�", "鐤簡", "琛�", "楠烽珔", "鏁叉墦", "鍐嶈", "鎿︽睏", "鎶犻蓟", "榧撴帉", "绯楀ぇ浜�", "鍧忕瑧", "宸﹀摷鍝�", "鍙冲摷鍝�", "鍝堟瑺", "閯欒", "濮斿眻", "蹇摥浜�", "闃撮櫓", "浜蹭翰", "鍚�", "鍙€�", "鑿滃垁", "瑗跨摐", "鍟ら厭", "绡悆", "涔掍箵", "鍜栧暋", "楗�", "鐚ご", "鐜懓", "鍑嬭阿", "鍢村攪", "鐖卞績", "蹇冪", "铔嬬硶", "闂數", "鐐稿脊", "鍒€", "瓒崇悆", "鐡㈣櫕", "渚夸究", "鏈堜寒", "澶槼", "绀肩墿", "鎷ユ姳", "寮�", "寮�", "鎻℃墜", "鑳滃埄", "鎶辨嫵", "鍕惧紩", "鎷冲ご", "宸姴", "鐖变綘", "NO", "OK", "鐖辨儏", "椋炲惢", "璺宠烦", "鍙戞姈", "鎬勭伀", "杞湀", "纾曞ご", "鍥炲ご", "璺崇怀", "鎶曢檷", "婵€鍔�", "涔辫垶", "鐚惢", "宸﹀お鏋�", "鍙冲お鏋�", "鍢垮搱", "鎹傝劯", "濂哥瑧", "鏈烘櫤", "鐨辩湁", "鑰�", "绾㈠寘", "楦�"];
-          //EmojiList= ["绗戣劯", "鐢熺梾", "鐮存稌涓虹瑧", "鍚愯垖", "鑴哥孩", "鎭愭儳", "澶辨湜", "鏃犺", "楝奸瓊", "鍚堝崄", "寮哄．", "搴嗙", "绀肩墿", "寮€蹇�", "澶х瑧", "鐑儏", "鐪ㄧ溂", "鑹�", "鎺ュ惢", "浜插惢", "闇查娇绗�", "婊℃剰", "鎴忓紕", "寰楁剰", "姹�", "浣庤惤", "鍛�", "鐒﹁檻", "鎷呭績", "闇囨儕", "鎮旀仺", "鐪兼唱", "鍝�", "鏅�", "蹇冪儲", "鐢熸皵", "鐫¤", "鎭堕瓟", "澶栨槦浜�", "蹇�", "蹇冪", "涓樻瘮鐗�", "闂儊", "鏄熸槦", "鍙瑰彿", "闂彿", "鐫＄潃", "姘存淮", "闊充箰", "鐏�", "渚夸究", "寮�", "鎷冲ご", "鑳滃埄", "涓�", "涓�", "鍙�", "宸�", "绗竴", "鍚�", "鐑亱", "鐢峰", "濂冲", "濂冲＋", "鐢峰＋", "澶╀娇", "楠烽珔", "绾㈠攪", "澶槼", "涓嬮洦", "澶氫簯", "闆汉", "鏈堜寒", "闂數", "娴锋氮", "鐚�", "灏忕嫍", "鑰侀紶", "浠撻紶", "鍏斿瓙", "鐙�", "闈掕洐", "鑰佽檸", "鑰冩媺", "鐔�", "鐚�", "鐗�", "閲庣尓", "鐚村瓙", "椹�", "铔�", "楦藉瓙", "楦�", "浼侀箙", "姣涜櫕", "绔犻奔", "楸�", "椴搁奔", "娴疯睔", "鐜懓", "鑺�", "妫曟鏍�", "浠欎汉鎺�", "绀肩洅", "鍗楃摐鐏�", "鍦ｈ癁鑰佷汉", "鍦ｈ癁鏍�", "绀肩墿", "閾�", "姘旂悆", "CD", "鐩告満", "褰曞儚鏈�", "鐢佃剳", "鐢佃", "鐢佃瘽", "瑙ｉ攣", "閿�", "閽ュ寵", "鎴愪氦", "鐏场", "閭", "娴寸几", "閽�", "鐐稿脊", "鎵嬫灙", "鑽父", "姗勬鐞�", "绡悆", "瓒崇悆", "妫掔悆", "楂樺皵澶�", "濂栨澂", "鍏ヤ镜鑰�", "鍞辨瓕", "鍚変粬", "姣斿熀灏�", "鐨囧啝", "闆ㄤ紴", "鎵嬫彁鍖�", "鍙ｇ孩", "鎴掓寚", "閽荤煶", "鍜栧暋", "鍟ら厭", "骞叉澂", "楦″熬閰�", "姹夊牎", "钖潯", "鎰忛潰", "瀵垮徃", "闈㈡潯", "鐓庤泲", "鍐版縺鍑�", "铔嬬硶", "鑻规灉", "椋炴満", "鐏", "鑷杞�", "楂橀搧", "璀﹀憡", "鏃�", "鐢蜂汉", "濂充汉", "O", "X", "鐗堟潈", "娉ㄥ唽鍟嗘爣", "鍟嗘爣"];
+            var str = '',
+                QQFaceList = ["微笑", "撇嘴", "色", "发呆", "得意", "流泪", "害羞", "闭嘴", "睡", "大哭", "尴尬", "发怒", "调皮", "呲牙", "惊讶", "难过", "酷", "冷汗", "抓狂", "吐", "偷笑", "愉快", "白眼", "傲慢", "饥饿", "困", "惊恐", "流汗", "憨笑", "悠闲", "奋斗", "咒骂", "疑问", "嘘", "晕", "疯了", "衰", "骷髅", "敲打", "再见", "擦汗", "抠鼻", "鼓掌", "糗大了", "坏笑", "左哼哼", "右哼哼", "哈欠", "鄙视", "委屈", "快哭了", "阴险", "亲亲", "吓", "可怜", "菜刀", "西瓜", "啤酒", "篮球", "乒乓", "咖啡", "饭", "猪头", "玫瑰", "凋谢", "嘴唇", "爱心", "心碎", "蛋糕", "闪电", "炸弹", "刀", "足球", "瓢虫", "便便", "月亮", "太阳", "礼物", "拥抱", "强", "弱", "握手", "胜利", "抱拳", "勾引", "拳头", "差劲", "爱你", "NO", "OK", "爱情", "飞吻", "跳跳", "发抖", "怄火", "转圈", "磕头", "回头", "跳绳", "投降", "激动", "乱舞", "献吻", "左太极", "右太极", "嘿哈", "捂脸", "奸笑", "机智", "皱眉", "耶", "红包", "鸡"];
+            //EmojiList= ["笑脸", "生病", "破涕为笑", "吐舌", "脸红", "恐惧", "失望", "无语", "鬼魂", "合十", "强壮", "庆祝", "礼物", "开心", "大笑", "热情", "眨眼", "色", "接吻", "亲吻", "露齿笑", "满意", "戏弄", "得意", "汗", "低落", "呸", "焦虑", "担心", "震惊", "悔恨", "眼泪", "哭", "晕", "心烦", "生气", "睡觉", "恶魔", "外星人", "心", "心碎", "丘比特", "闪烁", "星星", "叹号", "问号", "睡着", "水滴", "音乐", "火", "便便", "弱", "拳头", "胜利", "上", "下", "右", "左", "第一", "吻", "热恋", "男孩", "女孩", "女士", "男士", "天使", "骷髅", "红唇", "太阳", "下雨", "多云", "雪人", "月亮", "闪电", "海浪", "猫", "小狗", "老鼠", "仓鼠", "兔子", "狗", "青蛙", "老虎", "考拉", "熊", "猪", "牛", "野猪", "猴子", "马", "蛇", "鸽子", "鸡", "企鹅", "毛虫", "章鱼", "鱼", "鲸鱼", "海豚", "玫瑰", "花", "棕榈树", "仙人掌", "礼盒", "南瓜灯", "圣诞老人", "圣诞树", "礼物", "铃", "气球", "CD", "相机", "录像机", "电脑", "电视", "电话", "解锁", "锁", "钥匙", "成交", "灯泡", "邮箱", "浴缸", "钱", "炸弹", "手枪", "药丸", "橄榄球", "篮球", "足球", "棒球", "高尔夫", "奖杯", "入侵者", "唱歌", "吉他", "比基尼", "皇冠", "雨伞", "手提包", "口红", "戒指", "钻石", "咖啡", "啤酒", "干杯", "鸡尾酒", "汉堡", "薯条", "意面", "寿司", "面条", "煎蛋", "冰激凌", "蛋糕", "苹果", "飞机", "火箭", "自行车", "高铁", "警告", "旗", "男人", "女人", "O", "X", "版权", "注册商标", "商标"];
 
-          if ($('#' + id).length <= 0) {
-              str += '<div class="expression" id="' + id + '" style="display:none;border:#ccc 1px solid;position:absolute;z-index:99999;bottom:144px;background: white;">';
-              str += '<div class="scroll-wrapper">';
-              str += '<table class="face-tb">';
-              str += '</tr>';
-              for (var i = 0; i < QQFaceList.length; i++) {
-                  str += '<td><a href="javascript:void(0)" style="margin-left:5px;" title="' + QQFaceList[i] + '"><img src="' + settings.path + '/qqface/' + QQFaceList[i] + '.png"  class=\"imgFace\" data-name="' + QQFaceList[i] + '"/></a></td>';
-                  if ((i + 1) % 12 == 0) {
-                      str += '</tr><tr>'
-                  };
-              }
-              str += '</tr>';
-              str += '</table>';
-              str += '</div>';
+            if ($('#' + id).length <= 0) {
+                str += '<div class="expression" id="' + id + '" style="display:none;border:#ccc 1px solid;position:absolute;z-index:99999;bottom:144px;background: white;">';
+                str += '<div class="scroll-wrapper">';
+                str += '<table class="face-tb">';
+                str += '</tr>';
+                for (var i = 0; i < QQFaceList.length; i++) {
+                    str += '<td><a href="javascript:void(0)" style="margin-left:5px;" title="' + QQFaceList[i] + '"><img src="' + settings.path + '/qqface/' + QQFaceList[i] + '.png"  class=\"imgFace\" data-name="' + QQFaceList[i] + '"/></a></td>';
+                    if ((i + 1) % 12 == 0) {
+                        str += '</tr><tr>'
+                    };
+                }
+                str += '</tr>';
+                str += '</table>';
+                str += '</div>';
 
-          }
-          // $(option.appendbox).append(str);
-          $(this).parent().append(str);
+            }
+            // $(option.appendbox).append(str);
+            $(this).parent().append(str);
 
-          $(this).click(function (e) {
-              assign.focus();
-              $('#' + id).toggle();
-              e.stopPropagation();
-          });
+            $(this).click(function (e) {
+                assign.focus();
+                $('#' + id).toggle();
+                e.stopPropagation();
+            });
 
-          /* $(document).on("blur",'#'+option.assign,function (e) {
-               assign.focus();
-           });*/
+            /* $(document).on("blur",'#'+option.assign,function (e) {
+                 assign.focus();
+             });*/
 
-          $("#" + id + " table a").click(function (e) {
-              assign.focus();
-              insertHtmlAtCaret($(this).html());
-              e.stopPropagation();
-              $('#' + id).hide();
-          });
+            $("#" + id + " table a").click(function (e) {
+                assign.focus();
+                insertHtmlAtCaret($(this).html());
+                e.stopPropagation();
+                $('#' + id).hide();
+            });
 
-          function isIE() {
-              if (!!window.ActiveXObject || "ActiveXObject" in window) {
-                  return true;
-              } else {
-                  return false;
-              }
-          }
-          if (isIE()) {
-              function CheckHTML(Str) {
-                  var S = Str;
-                  S = S.replace(/<p(.*?>)/gi, "")
-                  S = S.replace(/<\/p>/gi, "")
-                  return S
-              }
+            function isIE() {
+                if (!!window.ActiveXObject || "ActiveXObject" in window) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            if (isIE()) {
+                function CheckHTML(Str) {
+                    var S = Str;
+                    S = S.replace(/<p(.*?>)/gi, "")
+                    S = S.replace(/<\/p>/gi, "")
+                    return S
+                }
 
-              $(document).on("mouseleave", '#' + option.assign, function (e) {
-                  assign.html(CheckHTML(assign.html()));
-              });
-          }
+                $(document).on("mouseleave", '#' + option.assign, function (e) {
+                    assign.html(CheckHTML(assign.html()));
+                });
+            }
 
-          $(document).click(function () {
-              $('#' + id).hide();
-          });
-      })
-  }
+            $(document).click(function () {
+                $('#' + id).hide();
+            });
+        })
+    }
 
 })(jQuery);
+
